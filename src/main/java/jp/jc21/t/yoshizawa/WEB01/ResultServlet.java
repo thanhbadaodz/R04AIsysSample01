bb71d7d7f4bb22e47a0bf9e7706bbda9c528547e
TUAN THANH NGUYEN <200648@jc-21.jp>
2022-10-31 11:56:32 +0900

    Fixed Sentiment Again

--- src/main/java/jp/jc21/t/yoshizawa/WEB01/ResultServlet.java
+++ src/main/java/jp/jc21/t/yoshizawa/WEB01/ResultServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class ResultServlet
  */
-@WebServlet("/result")
+@WebServlet({"/language_result","/sentiment_result"})
 public class ResultServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -44,8 +44,18 @@ public class ResultServlet extends HttpServlet {
 
 		try {
 			Language result = Json05.getLanguage(string);
-			String message = result.documents[0].detectedLanguage.name;
-			request.setAttribute("message","結果：" +message);
+			DetectedLanguage message = result.documents[0].detectedLanguage;
+			switch (request.getServletPath()) {
+				case "/language_result":
+					request.setAttribute("message","結果：" +message.name);
+					break;
+				case "/sentiment_result":
+					confidenceScores sentiment = Sentiment.getSentiment(string,message.iso6391Name).documents[0].confidenceScores;
+					String sentiment_text= "<br>Positive:"+ sentiment.positive
+							+ "<br>Neutral:" + sentiment.neutral
+							+ "<br>Negative:" + sentiment.negative;
+					request.setAttribute("message","結果：" +sentiment_text);
+				}
 			request.getRequestDispatcher("/WEB-INF/jsp/result.jsp").forward(request, response);
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
--- src/main/webapp/WEB-INF/jsp/languageRequest.jsp
+++ src/main/webapp/WEB-INF/jsp/languageRequest.jsp
@@ -11,7 +11,7 @@
 <h1>CognitiveServiceを使うサイト</h1>
 
 <h3>調べたい文字列を教えてください</h3>
-<form method="POST" action="./result">
+<form method="POST" action="./language_result">
 <input type="TEXT" name="string" />
 <input type="submit" />
 </form>
